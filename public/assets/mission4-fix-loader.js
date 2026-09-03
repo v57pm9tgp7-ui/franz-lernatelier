cdb716928ac8f89e5405b23d97e825c229ad18f9
v57pm9tgp7-ui <v57pm9tgp7@privaterelay.appleid.com>
2026-09-03 12:53:10 +0200

    Remove temporary Mission 04 fix files

--- public/assets/mission4-fix-loader.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  const src = '../../assets/mission4-fix-v2.js?v=2';
-  if(!document.querySelector(`script[src*="mission4-fix-v2.js"]`)){
-    const s = document.createElement('script');
-    s.src = src;
-    s.defer = true;
-    document.head.appendChild(s);
-  }
-})();
